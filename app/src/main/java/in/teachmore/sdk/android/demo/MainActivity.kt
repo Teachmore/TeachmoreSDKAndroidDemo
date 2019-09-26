67a935421548f627a9e84de9fc1b75fec138a3c2
Harshal Bhakta <harshal.c.bhakta@gmail.com>
2019-09-26 20:32:42 +0530

    Launch in separate activity works

--- app/src/main/java/in/teachmore/sdk/android/demo/MainActivity.kt
+++ app/src/main/java/in/teachmore/sdk/android/demo/MainActivity.kt
@@ -1,5 +1,6 @@
 package `in`.teachmore.sdk.android.demo
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -7,6 +8,7 @@ import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,9 +17,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        button_launch_teachmore_in_separate_activity.setOnClickListener { view ->
+            val intent = Intent(this, `in`.teachmore.androidsdk.activities.HomeActivity::class.java)
+            startActivity(intent)
         }
     }
 
